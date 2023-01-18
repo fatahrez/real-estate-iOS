b14b23edfb812cbaf218e4157d4ddfedba07b7c1
fatahrez <fertahrez@gmail.com>
2023-01-18 20:12:48 +0300

    feature: Consume endpoint for Property Details

--- Guryihii/controller/PropertyDetailsViewController.swift
+++ Guryihii/controller/PropertyDetailsViewController.swift
@@ -12,15 +12,30 @@ class PropertyDetailsViewController : UIViewController {
     
     @IBOutlet weak var slugLabel: UILabel!
     
+    var propertyDetailManager = PropertyDetailManager()
     var slug: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        propertyDetailManager.delegate = self
+        propertyDetailManager.fetchPropertyDetails(with: slug ?? "")
         slugLabel.text = slug
     }
+}
+
+extension PropertyDetailsViewController : PropertyDetailManagerDelegate {
     
-    override func dismiss(animated flag: Bool, completion: (() -> Void)? = nil) {
-        slug = ""
+    func didGetPropertyDetails(_ propertyDetailManager: PropertyDetailManager, _ property: PropertyModel) {
+        
+        DispatchQueue.main.async {
+            print(property)
+        }
     }
+    
+    func didFailWith(error: Error) {
+        print(error)
+    }
+    
+    
 }
