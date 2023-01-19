8a6ab3623e63eb7aaa4025e5f9d6b6f7a1b35a2f
fatahrez <fertahrez@gmail.com>
2023-01-19 18:32:41 +0300

    feature: Add Navbar and details

--- Guryihii/controller/PropertyDetailsViewController.swift
+++ Guryihii/controller/PropertyDetailsViewController.swift
@@ -8,12 +8,16 @@
 import Foundation
 import UIKit
 
-class PropertyDetailsViewController : UIViewController {
+class PropertyDetailsViewController : UIViewController, iCarouselDataSource,
+                                      iCarouselDelegate{
     
+    @IBOutlet weak var propertyImageSlideView: iCarousel!
     @IBOutlet weak var slugLabel: UILabel!
     
     var propertyDetailManager = PropertyDetailManager()
     var slug: String?
+    var property: PropertyModel?
+    var images = [UIImage]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +25,39 @@ class PropertyDetailsViewController : UIViewController {
         propertyDetailManager.delegate = self
         propertyDetailManager.fetchPropertyDetails(with: slug ?? "")
         slugLabel.text = slug
+    
+        images.append(UIImage(named: "house_placeholder")!)
+        
+        propertyImageSlideView.layer.cornerRadius = 10
+        
+        propertyImageSlideView.reloadData()
+        propertyImageSlideView.type = .timeMachine
+    }
+    
+    func numberOfItems(in carousel: iCarousel) -> Int {
+        return images.count
+    }
+    
+    func carousel(_ carousel: iCarousel, viewForItemAt index: Int, reusing view: UIView?) -> UIView {
+        print(images)
+        let tempView = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 250))
+        
+        let frame = CGRect(x: 0, y: 0, width: 200, height: 250)
+        let imageView = UIImageView()
+        imageView.frame = frame
+        imageView.contentMode = .scaleAspectFit
+        
+        imageView.image = images[index]
+        
+        return tempView
+    }
+    
+    func carousel(_ carousel: iCarousel, valueFor option: iCarouselOption, withDefault value: CGFloat) -> CGFloat {
+        if option == iCarouselOption.spacing {
+            print(value)
+            return value * 1.2
+        }
+        return value
     }
 }
 
@@ -28,8 +65,16 @@ extension PropertyDetailsViewController : PropertyDetailManagerDelegate {
     
     func didGetPropertyDetails(_ propertyDetailManager: PropertyDetailManager, _ property: PropertyModel) {
         
-        DispatchQueue.main.async {
-            print(property)
+        DispatchQueue.global(qos: .background).async {
+            do {
+                let data = try Data.init(contentsOf: URL.init(string: K.baseURLWithoutAPI+property.cover_photo)!)
+                DispatchQueue.main.async {
+                    let image: UIImage = UIImage(data: data)!
+                    self.images.append(image)
+                }
+            } catch {
+                print(error)
+            }
         }
     }
     
