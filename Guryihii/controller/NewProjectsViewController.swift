5f7e044afb8e87838bc131bd0ab2f410d25d3300
fatahrez <fertahrez@gmail.com>
2023-02-02 11:30:31 +0300

    feat: finish on property details

--- Guryihii/controller/NewProjectsViewController.swift
+++ Guryihii/controller/NewProjectsViewController.swift
@@ -14,7 +14,6 @@ class NewProjectsViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        newProjectsLabel.text = "New Projects VC"
     }
     
 
